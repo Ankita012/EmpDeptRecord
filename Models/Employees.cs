40931b234f4edec46c30cdf45f7446de242b3fa5
Ankita <Ankita012@users.noreply.github.com>
2024-02-16 23:04:52 +1300

    Commiting all the code

---
+++ Models/Employees.cs
@@ -0,0 +1,12 @@
+﻿namespace Employee.Models
+{
+    public class Employees
+    {
+        public int EmployeeId { get; set; }
+        public string? EmployeeName { get; set; }
+        public string? Department { get; set; }
+        public DateOnly DateOfJoining { get; set; }
+        public string? PhotoFileName { get; set; }
+
+    }
+}
